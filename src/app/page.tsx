03ff26c27b4937287d1955ef447db12f0e645096
tatsuki <71578955+kubotatsuki@users.noreply.github.com>
2025-08-16 05:48:05 +0800

    feat: Create landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+'use client'
+
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
+
 const Page = () => {
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({
+    onSuccess: () => {
+      toast.success("Background job started")
+    }
+  }))
+  
   return ( 
-    <div className="font-bold text-rose-500">
-      <h1>Hello Vibe</h1>
+    <div className="p-4 max-w-7xl mx-auto">
+      <Button disabled={invoke.isPending} onClick={() => invoke.mutate({ text: "John" })}>
+        Invoke Background Job
+      </Button>
     </div>
    );
 }
